b4f47fcf3102327797398741465fd0db20be6e1f
JakeWalker <JakeWalker@DESKTOP-SOJ2S5O>
2024-05-30 20:10:36 +0100

    Added test to validate empty JSON object

---
+++ test/test_parser.py
@@ -0,0 +1,13 @@
+import pytest
+
+'''
+# Step 1
+In this step your goal is to parse a valid simple JSON object, specifically: ‘{}’ 
+and an invalid JSON file and correctly report which is which. 
+So you should build a very simple lexer and parser for this step.
+'''
+
+def test_parser_returns_true_with_valid_JSON_object():
+    data = {}
+    
+    assert Parser.parse_json(data) == True
