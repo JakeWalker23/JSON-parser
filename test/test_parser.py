7321fe68ce52f66eaddf6f19cd238c8652c4504c
Thomas Dittmer <217936+TheDitt@users.noreply.github.com>
2024-07-05 23:16:55 +0100

    added failing test for empty array which is valid json

--- test/test_parser.py
+++ test/test_parser.py
@@ -117,3 +117,10 @@ def test_parser_returns_correct_values_for_None_values():
     assert result['name'] == 'Jake'
     assert result['age'] == 23
     assert result['bellend'] is None
+
+def test_for_valid_json_array():
+    data = '[]'
+
+    result = Parser.parse_json_to_object(data)
+
+    assert result == []
